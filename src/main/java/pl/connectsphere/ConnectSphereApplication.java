16651da8afeef5d621b664fd43f271446e350cea
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-23 13:56:49 +0200

    Project setup.

---
+++ src/main/java/pl/connectsphere/ConnectSphereApplication.java
@@ -0,0 +1,13 @@
+package pl.connectsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/connectsphere/ConnectSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.connectsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

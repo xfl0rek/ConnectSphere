0f01b88c4c3278f53763f05465a4ab00ca9e8d03
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-25 15:18:57 +0200

    Added User class.

---
+++ src/main/java/pl/connectsphere/model/User.java
@@ -0,0 +1,50 @@
+package pl.connectsphere.model;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

c4aa1b84c8c9c85c0eb974d7d19f6400f93727b5
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-25 15:22:41 +0200

    Aded UserRepository class.

---
+++ src/main/java/pl/connectsphere/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package pl.connectsphere.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.connectsphere.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+}

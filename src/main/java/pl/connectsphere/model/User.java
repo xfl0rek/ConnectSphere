de105ea3a7d9d0ad6ba7f1bc78f32d6bd667ada9
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-25 15:24:21 +0200

    Added entity adnotation in User class.

--- src/main/java/pl/connectsphere/model/User.java
+++ src/main/java/pl/connectsphere/model/User.java
@@ -1,6 +1,14 @@
 package pl.connectsphere.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;

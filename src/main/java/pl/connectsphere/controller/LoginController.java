bdcb14cfbf2f87c4c221c272e72748c03898c99b
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-25 14:49:50 +0200

    Added LoginController class.

---
+++ src/main/java/pl/connectsphere/controller/LoginController.java
@@ -0,0 +1,14 @@
+package pl.connectsphere.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    @GetMapping
+    private String login() {
+        return "login";
+    }
+}

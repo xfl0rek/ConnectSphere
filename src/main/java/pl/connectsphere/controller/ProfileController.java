34330e28abdc651c5cb0e8d5d5ad1d63d4356d51
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-26 13:48:57 +0200

    Updated ProfileControler.

--- src/main/java/pl/connectsphere/controller/ProfileController.java
+++ src/main/java/pl/connectsphere/controller/ProfileController.java
@@ -1,13 +1,25 @@
 package pl.connectsphere.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.connectsphere.model.User;
+import pl.connectsphere.repository.PostRepository;
 
 @Controller
 public class ProfileController {
+    private final PostRepository postRepository;
+
+    public ProfileController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @GetMapping("/profile")
-    public String profile(Model model) {
+    public String profile(Model model, HttpSession session) {
+        User loggedInUser = (User) session.getAttribute("user");
+        model.addAttribute("loggedInUser", loggedInUser);
+        model.addAttribute("posts", postRepository.findByUserId(loggedInUser.getId()));
         return "profile";
     }
 }

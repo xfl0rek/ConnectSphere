5e079ffbceadd123155bf2c0b3600dcc2ef4f381
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-26 13:31:00 +0200

    Added simple profile in html and css and added ProfileController.

---
+++ src/main/java/pl/connectsphere/controller/ProfileController.java
@@ -0,0 +1,13 @@
+package pl.connectsphere.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        return "profile";
+    }
+}

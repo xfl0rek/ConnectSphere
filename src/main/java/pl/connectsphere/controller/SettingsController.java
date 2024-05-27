7be1ea1ed84e4fcc8e5cc223ef8aab73496535f4
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-27 12:14:23 +0200

    Implemented SettingsController and code refactor.

--- src/main/java/pl/connectsphere/controller/SettingsController.java
+++ src/main/java/pl/connectsphere/controller/SettingsController.java
@@ -1,15 +1,29 @@
 package pl.connectsphere.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.connectsphere.model.User;
+import pl.connectsphere.repository.UserRepository;
 
 @Controller
 @RequestMapping("/settings")
 public class SettingsController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SettingsController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping
-    public String showSettingsForm() {
+    public String showSettingsForm(Model model, HttpSession session) {
+        User loggedInUser = (User) session.getAttribute("user");
+        model.addAttribute("loggedInUser", loggedInUser);
+        model.addAttribute("userInfo", userRepository.findByEmail(loggedInUser.getEmail()));
         return "settings";
     }
 }

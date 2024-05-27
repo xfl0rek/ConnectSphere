05d0bc1e3449ff3eed7ea23aef64f1bbf1e01240
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-27 20:32:41 +0200

    Added LogoutController class in controller package. Logging out is working correctly.

---
+++ src/main/java/pl/connectsphere/controller/LogoutController.java
@@ -0,0 +1,21 @@
+package pl.connectsphere.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/logout")
+public class LogoutController {
+
+    @GetMapping
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
+}
--- src/main/java/pl/connectsphere/controller/RegisterController.java
+++ src/main/java/pl/connectsphere/controller/RegisterController.java
@@ -2,7 +2,6 @@ package pl.connectsphere.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;

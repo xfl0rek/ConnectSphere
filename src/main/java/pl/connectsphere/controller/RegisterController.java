7a8d99179fcdf00082cea72350843575308b7c73
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-28 16:21:01 +0200

    Added alerts for registration process.

--- src/main/java/pl/connectsphere/controller/RegisterController.java
+++ src/main/java/pl/connectsphere/controller/RegisterController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.connectsphere.model.User;
 import pl.connectsphere.repository.UserRepository;
 
@@ -19,12 +20,17 @@ public class RegisterController {
     }
 
     @PostMapping
-    public String registerUser(@RequestParam String name, @RequestParam String email, @RequestParam String password) {
+    public String registerUser(@RequestParam String name,
+                               @RequestParam String email,
+                               @RequestParam String password,
+                               RedirectAttributes redirectAttributes) {
         if (userRepository.existsByName(name)) {
+            redirectAttributes.addFlashAttribute("message", "Username already exists!");
             return "redirect:/";
         }
 
         if (userRepository.existsByEmail(email)) {
+            redirectAttributes.addFlashAttribute("message", "Email already in use!");
             return "redirect:/";
         }
         User user = new User(name, email, password);

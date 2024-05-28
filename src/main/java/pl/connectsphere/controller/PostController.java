2ab1eceb4edbfcc1dfc83fe2bc203ad7c2cd7174
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-28 16:08:47 +0200

    Code refactor; added post alert.

--- src/main/java/pl/connectsphere/controller/PostController.java
+++ src/main/java/pl/connectsphere/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.connectsphere.model.Post;
 import pl.connectsphere.model.User;
 import pl.connectsphere.repository.PostRepository;
@@ -23,12 +24,13 @@ public class PostController {
     }
 
     @PostMapping("/addPost")
-    public String addPost(@RequestParam String content, HttpSession session) {
+    public String addPost(@RequestParam String content, HttpSession session, RedirectAttributes redirectAttributes) {
         User user = (User) session.getAttribute("user");
         if (user == null) {
             return "redirect:/";
         }
         if (content.isEmpty() || content.trim().isEmpty()) {
+            redirectAttributes.addFlashAttribute("postError", "Your post is empty!");
             return "redirect:/home";
         }
         LocalDateTime now = LocalDateTime.now();
